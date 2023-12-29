aa8abe6c585f21519b9d4c528bd1db894e8fc1db
Eliaexe <eliasidori@outlook.it>
2023-12-29 20:01:31 +0100

    commented

--- config/passport-config.js
+++ config/passport-config.js
@@ -11,7 +11,7 @@ const authType = ['snapchat']
 // ,'instagram', 'google', 'facebook', 'apple'
 
 const authenticationStrategies = authType.map(e => {
-  const strategy = require(`${e.charAt(0).toUpperCase() + e.slice(1).toLowerCase()}Strategy`);
+  // const strategy = require(`${e.charAt(0).toUpperCase() + e.slice(1).toLowerCase()}Strategy`);
   return {
     name: e,
     strategy: strategy,
