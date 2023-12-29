df1c126776b393ebefd465b3461734f8f887b498
Eliaexe <eliasidori@outlook.it>
2023-12-29 20:04:37 +0100

    no map

--- config/passport-config.js
+++ config/passport-config.js
@@ -1,42 +1,30 @@
-// passport-config.js
-const InstagramStrategy = require('passport-instagram').Strategy;
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const SnapchatStrategy = require('passport-snapchat').Strategy;
-const FacebookStrategy = require('passport-facebook').Strategy;
-const AppleStrategy = require('passport-apple').Strategy;
-const { each } = require('mongoose/lib/utils');
-const User = require('../models/User'); 
+const User = require('../models/User');
 
-const authType = ['snapchat']
-// ,'instagram', 'google', 'facebook', 'apple'
-
-const authenticationStrategies = authType.map(e => {
-  // const strategy = require(`${e.charAt(0).toUpperCase() + e.slice(1).toLowerCase()}Strategy`);
-  return {
-    name: e,
-    strategy: strategy,
+const authenticationStrategies = [
+  {
+    name: 'snapchat',
+    strategy: SnapchatStrategy,
     options: {
-      clientID: process.env[`PUBLIC_${e.toUpperCase()}_ID`],
-      clientSecret: process.env[`PRIVATE_${e.toUpperCase()}_KEY`],
-      callbackURL: `${process.env.CLIENT_URL}auth/${e}/callback`
+      clientID: process.env.PUBLIC_SNAPCHAT_ID,
+      clientSecret: process.env.PRIVATE_SNAPCHAT_KEY,
+      callbackURL: `${process.env.CLIENT_URL}auth/snapchat/callback`,
     },
     callback: (accessToken, refreshToken, profile, done) => {
-      // Implementa la logica per trovare o creare l'utente nel database
-      const socialId = `${e}Id`;
+      // Implement the logic to find or create the user in the database
+      const socialId = 'snapchatId';
       const query = {};
       query[socialId] = profile.id;
 
       User.findOneOrCreate(query, function (err, user) {
         return done(err, user);
       });
-    }
-  };
-});
+    },
+  },
+];
 
 module.exports = function (passport) {
   authenticationStrategies.forEach(({ name, strategy, options, callback }) => {
-    passport.use(
-      new strategy(options, callback)
-    );
+    passport.use(new strategy(options, callback));
   });
 };
