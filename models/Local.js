131e025c19636484cd0b01c2ca9945ca1fc73647
Eliaexe <eliasidori@outlook.it>
2024-01-03 18:42:05 +0100

    try to add the product directly to inventory

--- models/Local.js
+++ models/Local.js
@@ -23,11 +23,48 @@ const LocalSchema = new mongoose.Schema(
         type: String,
         default: '/uploads/example.jpeg',
       },
-      inventory: {
-        type: [ProductSchema],
-        default: [],
-        required: true
-      },
+      inventory: [
+        {
+          name: {
+            type: String,
+            trim: true,
+            required: [true, 'Please provide product name'],
+            maxlength: [100, 'Name can not be more than 100 characters'],
+          },
+          price: {
+            type: Number,
+            required: [true, 'Please provide product price'],
+            default: 0,
+          },
+          description: {
+            type: String,
+            required: [true, 'Please provide product description'],
+            maxlength: [1000, 'Description can not be more than 1000 characters'],
+          },
+          image: {
+            type: String,
+            default: '/uploads/example.jpeg',
+          },
+          category: {
+            type: String,
+            required: [true, 'Please provide product category'],
+            enum: ['drink', 'soft', 'cocktails', 'ticket'],
+          },
+          company: {
+            type: String,
+            required: [true, 'Please provide company'],
+          },
+          quantity: {
+            type: Number,
+            required: true,
+            default: 15,
+          },
+          id: {
+            type: mongoose.Types.ObjectId,
+            required: true,
+          },
+        },
+      ],
       owner: {
         type: mongoose.Types.ObjectId,
         ref: 'User',
