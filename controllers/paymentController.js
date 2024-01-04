4b8b4d26f5faea541a255e976faec42c2ecb4e05
Eliaexe <eliasidori@outlook.it>
2024-01-04 05:12:25 +0100

    testing payment whit paytweak

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,32 +1,52 @@
+const https = require('https');
+
 const createPayment = (req, res) => {
     const { order_id, amount } = req.body;
-    const apiUrl = 'https://api.paytweak.dev/v1/links/';
+    const apiUrl = 'https://api.paytweak.dev/v1/links';
+    const workToken = process.env.PAYTWEAK_SECRET_KEY;
 
-    const requestBody = {
+    const postData = JSON.stringify({
         order_id: order_id,
         amount: amount,
         lang: 'FR',
         cur: 'EUR',
+    });
+
+    const options = {
+        hostname: 'api.paytweak.dev',
+        port: 443,
+        path: '/v1/links',
+        method: 'POST',
+        headers: {
+            'Paytweak-Token': workToken,
+            'Content-Type': 'application/json',
+            'Content-Length': postData.length,
+        },
+        rejectUnauthorized: true,
     };
 
-    if (typeof fetch !== 'undefined') {
-        fetch(apiUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(requestBody),
-        })
-        .then(response => response.json())
-        .then(data => {
-            res.status(200).json({ status: 'ok', data });
-        })
-        .catch(error => {
-            res.status(500).json({ status: 'error', error: error.message });
+    const request = https.request(options, (response) => {
+        let data = '';
+
+        response.on('data', (chunk) => {
+            data += chunk;
         });
-    } else {
-        res.status(500).json({ status: 'error', error: 'Ambiente non supporta l\'API fetch' });
-    }
+
+        response.on('end', () => {
+            if (response.statusCode === 403) {
+                res.status(403).json({ status: 'error', message: 'Accesso non autorizzato' });
+            } else {
+                res.status(200).json({ status: 'ok', data });
+            }
+        });
+    });
+
+    request.on('error', (error) => {
+        res.status(500).json({ status: 'error', error: error.message });
+    });
+
+    request.write(postData);
+    request.end();
 };
 
 module.exports = {
